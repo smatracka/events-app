dfda8e4d5418a4d5bc7ba8a6bc42ac461adf0991
smatracka <saramatracka@gmail.com>
2020-08-22 19:42:57 +0200

    Added event thumbnail component and pass data from event list.

---
+++ src/app/events/event-thumbnail/event-thumbnail.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-event-thumbnail',
+  templateUrl: './event-thumbnail.component.html',
+  styleUrls: ['./event-thumbnail.component.css']
+})
+export class EventThumbnailComponent  {
+  @Input() event: any;
+
+}
