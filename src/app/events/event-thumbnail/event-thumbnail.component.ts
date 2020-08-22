32c44e85c42ae623d850c8a420feac11a986d9d7
smatracka <saramatracka@gmail.com>
2020-08-22 21:25:33 +0200

    Testing buttons.

--- src/app/events/event-thumbnail/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail/event-thumbnail.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-event-thumbnail',
@@ -8,4 +8,7 @@ import {Component, Input, OnInit} from '@angular/core';
 export class EventThumbnailComponent  {
   @Input() event: any;
 
+  logFoo(){
+    console.log('foo');
+  }
 }
