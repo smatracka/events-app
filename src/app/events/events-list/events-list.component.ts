c08936969d8cab2f09b37b6e6795d03f6898d2c0
smatracka <saramatracka@gmail.com>
2020-08-22 19:24:36 +0200

    Added event list component and fonts.

--- src/app/events/events-list/events-list.component.ts
+++ src/app/events/events-list/events-list.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-events-list',
+  selector: 'events-list',
   templateUrl: './events-list.component.html',
   styleUrls: ['./events-list.component.css']
 })
-export class EventsListComponent implements OnInit {
+export class EventsListComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  event = {
+    id: 1,
+    name: 'Angular Connect',
+    date: '30.10.2020',
+    time: '10:00 am',
+    price: 599.99,
+    imageUrl: 'assets/img/angularconnect-shield.png',
+    location: {
+      address: 'al. Jerozolimskie 33',
+      city: 'Warsaw',
+      country: 'Poland'
+    }
+  };
 
 }
